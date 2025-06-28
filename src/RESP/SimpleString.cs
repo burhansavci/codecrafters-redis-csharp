dd3ecbe7aead3d4936d2d1759b9e4a6b471d59b3
burhansavci <burhansavci@gmail.com>
2025-06-28 18:20:57 +0300

    Implement basic `SET` and `GET` commands for Redis server.

--- src/RESP/SimpleString.cs
+++ src/RESP/SimpleString.cs
@@ -2,9 +2,11 @@ namespace codecrafters_redis.RESP;
 
 public record SimpleString(string Data) : RespObject(DataType.SimpleString)
 {
+    public static SimpleString Ok => new("OK");
     public static implicit operator SimpleString(string data) => new(data);
 
     public static implicit operator string(SimpleString simpleString) => simpleString.ToString();
+    
 
     public override string ToString()
     {
