b67c22d073623731a7f629e51922f789e56ec61e
burhansavci <burhansavci@gmail.com>
2025-07-09 16:40:42 +0300

    Add RDB-based fallback for `GET` command to retrieve keys from disk when not found in memory

--- src/Commands/GetCommand.cs
+++ src/Commands/GetCommand.cs
@@ -1,5 +1,6 @@
 using System.Net.Sockets;
 using System.Text;
+using codecrafters_redis.Rdb;
 using codecrafters_redis.RESP;
 
 namespace codecrafters_redis.Commands;
@@ -18,6 +19,14 @@ public class GetCommand(Server server) : ICommand
             throw new FormatException("Invalid key format. Expected bulk string.");
 
         var value = server.Db.TryGetValue(key.Data!, out var record) ? new BulkString(record.Value) : new BulkString(null);
+
+        if (value.Data is null)
+        {
+            using var reader = new RdbReader(server.DbDirectory, server.DbFileName);
+            var db = reader.Read();
+
+            value = db.TryGetValue(key.Data!, out var rawValue) ? new BulkString(rawValue) : new BulkString(null);
+        }
 
         await connection.SendAsync(Encoding.UTF8.GetBytes(value));
     }
