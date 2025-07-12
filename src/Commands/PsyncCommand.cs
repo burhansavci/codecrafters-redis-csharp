0e4a22ba201bc215fe9d28c5ec42d62adb0e0217
burhansavci <burhansavci@gmail.com>
2025-07-12 19:00:59 +0300

    Enhance `PSYNC` command to validate arguments and include master replication ID and offset

--- src/Commands/PsyncCommand.cs
+++ src/Commands/PsyncCommand.cs
@@ -4,13 +4,23 @@ using codecrafters_redis.RESP;
 
 namespace codecrafters_redis.Commands;
 
-public class PsyncCommand : ICommand
+public class PsyncCommand(Server.Server server) : ICommand
 {
     public const string Name = "PSYNC";
 
     public async Task Handle(Socket connection, RespObject[] args)
     {
-        var response = new SimpleString("FULLRESYNC");
+        ArgumentNullException.ThrowIfNull(args);
+        ArgumentOutOfRangeException.ThrowIfZero(args.Length);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(args.Length, 2);
+        
+        if (args[0] is not BulkString replicationId)
+            throw new FormatException("Invalid replication id format. Expected bulk string.");
+        
+        if (args.Length > 1 && args[1] is not BulkString replicationOffset)
+            throw new FormatException("Invalid replication offset format. Expected bulk string.");
+        
+        var response = new SimpleString($"FULLRESYNC {server.MasterReplicationId} {server.MasterReplicationOffset}");
 
         await connection.SendAsync(Encoding.UTF8.GetBytes(response));
     }
