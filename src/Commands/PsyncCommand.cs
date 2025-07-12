4e863e4dae008fe984759f4e3dec2cfd46d0c89a
burhansavci <burhansavci@gmail.com>
2025-07-12 19:22:35 +0300

    Send empty RDB file on full resync

--- src/Commands/PsyncCommand.cs
+++ src/Commands/PsyncCommand.cs
@@ -14,15 +14,29 @@ public class PsyncCommand(RedisServer redisServer) : ICommand
         ArgumentNullException.ThrowIfNull(args);
         ArgumentOutOfRangeException.ThrowIfZero(args.Length);
         ArgumentOutOfRangeException.ThrowIfGreaterThan(args.Length, 2);
-        
+        ArgumentOutOfRangeException.ThrowIfLessThan(args.Length, 1);
+
         if (args[0] is not BulkString replicationId)
             throw new FormatException("Invalid replication id format. Expected bulk string.");
-        
-        if (args.Length > 1 && args[1] is not BulkString replicationOffset)
+
+        if (replicationId.Data != "?")
+            throw new FormatException("Invalid replication id. Expected '?'");
+
+        if (args[1] is not BulkString replicationOffset)
             throw new FormatException("Invalid replication offset format. Expected bulk string.");
-        
+
+        if (replicationOffset.Data != "-1")
+            throw new FormatException("Invalid replication offset. Expected '-1'");
+
         var response = new SimpleString($"FULLRESYNC {redisServer.MasterReplicationId} {redisServer.MasterReplicationOffset}");
 
         await connection.SendAsync(Encoding.UTF8.GetBytes(response));
+
+        var rdbBinaryData = Convert.FromBase64String(RedisServer.DefaultEmptyRdbFileInBase64);
+        var emptyRdbFileResponse = $"${rdbBinaryData.Length}\r\n";
+
+        await connection.SendAsync(Encoding.UTF8.GetBytes(emptyRdbFileResponse));
+
+        await connection.SendAsync(rdbBinaryData);
     }
 }
