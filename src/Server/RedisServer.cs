9c155c9d3731cded77f8e6e66f01ec6982edf6dc
burhansavci <burhansavci@gmail.com>
2025-07-12 19:22:35 +0300

    Send empty RDB file on full resync

--- src/Server/RedisServer.cs
+++ src/Server/RedisServer.cs
@@ -13,6 +13,7 @@ public class RedisServer
     private const string DefaultPort = "6379";
     private const string DefaultMasterReplicationId = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb";
     private const int DefaultMasterReplicationOffset = 0;
+    public const string DefaultEmptyRdbFileInBase64 = "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==";
     private const string MasterRole = "master";
     private const string SlaveRole = "slave";
     private const int BufferSize = 4 * 1024;
